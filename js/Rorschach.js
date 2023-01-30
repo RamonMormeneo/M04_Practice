6d1c85ba0362e99579d81e9a508b1901171472c8
Ramon Mormeneo <almiond5@gmail.com>
2023-01-30 07:55:05 +0100

    Rorsharck Main

---
+++ js/Rorschach.js
@@ -0,0 +1,66 @@
+function B1()
+{
+    var x = document.getElementById("Text1");
+    var y = document.getElementById("Text2");
+    var z = document.getElementById("Text3");
+    var j = document.getElementById("Text4");
+
+    var img = document.getElementById("imgR");
+    img.src="img/rorschach1.jpg"
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+    j.style.display = "none";
+
+}
+
+function B2()
+{
+    var x = document.getElementById("Text1");
+    var y = document.getElementById("Text2");
+    var z = document.getElementById("Text3");
+    var j = document.getElementById("Text4");
+
+    var img = document.getElementById("imgR");
+    img.src="img/Rosharck6.jpg"
+
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "none";
+    j.style.display = "none";
+
+}
+
+function B3()
+{
+    var x = document.getElementById("Text1");
+    var y = document.getElementById("Text2");
+    var z = document.getElementById("Text3");
+    var j = document.getElementById("Text4");
+
+    var img = document.getElementById("imgR");
+    img.src="img/Rosharck7.jpg"
+
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "block";
+    j.style.display = "none";
+
+}
+
+function B4()
+{
+    var x = document.getElementById("Text1");
+    var y = document.getElementById("Text2");
+    var z = document.getElementById("Text3");
+    var j = document.getElementById("Text4");
+
+    var img = document.getElementById("imgR");
+    img.src="img/Rosharck8.jpg"
+
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "none";
+    j.style.display = "block";
+
+}
